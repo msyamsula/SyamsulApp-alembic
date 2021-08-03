e04726c254972ead247ee5a701f1ade16d3b0bc7
msyamsula <msyamsula1995@gmail.com>
2021-08-03 23:05:57 +0700

    shop table

--- models/shop.py
+++ models/shop.py
@@ -11,6 +11,4 @@ class Shop(Base):
     created_at = Column(TIMESTAMP, server_default=func.now())
     updated_at = Column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
 
-    # __table_args__ = (
-    #     Index("owner_id", "owner_id")
-    # )
+    __table_args__ = ()
